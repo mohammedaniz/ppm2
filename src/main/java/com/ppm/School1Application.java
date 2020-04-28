ca51f5e2de796c290d44450a1f96e1116dd2025a
= <43546339+mohammedaniz@users.noreply.github.com>
2020-04-28 11:16:17 +0530

    Project Management tool Backend

---
+++ src/main/java/com/ppm/School1Application.java
@@ -0,0 +1,13 @@
+package com.ppm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(School1Application.class, args);
+	}
+
+}
